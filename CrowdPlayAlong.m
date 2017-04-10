e873fc9c98de82fcdab7a01ec995d7cadeb93881
DebrajGhose <dg.chaos06@gmail.com>
2017-04-10 19:57:19 -0400

    Made tempo a variable in the program, but it feels a bit fast

--- CrowdPlayAlong.m
+++ CrowdPlayAlong.m
@@ -5,18 +5,22 @@ close all
 
 %set up video writing stuff
 
-video = VideoWriter('Test.avi');
+video = VideoWriter('Song3_60.avi');
 open(video);
 
 figure1 = figure('Position', [0 , 0 , 2300 , 1400 ]);
 
 %Set up simulation parameters
 
+tempo = 60; %in BPM
+smallestunit = 1/4; %smallest note
+
 length = 200; %size of your domain
-drawfreq = 30; %frequency with which it draws stuff till a new note can appear
-lowerby = 20/drawfreq; %amount by which you want the notes to be lowered
+drawfreq = 30*60/(tempo/smallestunit/4); %frequency with which it draws stuff till a new note can appear for a video of 30 frames/sec
+visdiff = 20; %this number of pixels by which shapes should be separated in the smallest time unit
+lowerby = visdiff/drawfreq; %amount by which you want the notes to be lowered
 shapesize = 1000;
-disappear_height = 180; %height at which the shape should disappear
+disappear_height = 20; %height at which the shape should disappear
 noteshape = '<o>s^dvhhp'; %shapes of your notes
 notecolor = { [0.1 0.1 0.7] [0.1 0.7 0.2] [0.7 0.2 0.7] [0.7 0.3 0.1] [0.6 0.7 0.2] ...
     [0.5 0.2 0.9] [0.9 0.3 0.7] [0.4 0.9 0.7] [0.2 0.9 0.2] [0.3 0.4 0.9]  }; %colors of your notes
@@ -30,13 +34,20 @@ notes = { [] [] [] [] [] [] [] [] [] [] }; %first 8 cells hold notes in an octav
 
 %load input array
 
-input = [ 1 0 1 0 0 0 1 1 0 1; 1 0 0 0 0 0 1 1 1 0 ; 0 1 0 0 0 0 1 1 0 1; 1 1 1 1 1 1 1 1 1 1];
+%input = importdata('Song1.txt');
+
+input = importdata('Song3.txt');
+
+
+%input = [ 1 0 1 0 0 0 1 1 0 1; 1 0 0 0 0 0 1 1 1 0 ; 0 1 0 0 0 0 1 1 0 1; 1 1 1 1 1 1 1 1 1 1];
 
 % loop to generate your video
 
 sizeofinput = numel(input(:,1)); %number of things (notes) that are going to be fed
 
 for i = 1:(sizeofinput + round(length/lowerby)) %the simulation will run for as long as it takes the last note to reach the bottom of the screen
+
+    %% input stuff from the song matrix
     
     if i <= sizeofinput %there will be an error if this condition is not there
         
@@ -50,6 +61,8 @@ for i = 1:(sizeofinput + round(length/lowerby)) %the simulation will run for as 
         end
         
     end
+    
+    %% control display dynamics
     
     for loweri = 1:drawfreq %lower notes in the domain y
         
@@ -108,7 +121,6 @@ for i = 1:(sizeofinput + round(length/lowerby)) %the simulation will run for as 
             end
             
         end
-        
         
         %plot properties
         
